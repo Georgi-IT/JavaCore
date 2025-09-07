85811938d4ec93bcd780b8ea40bc55cab7ca089d
sky <gogohayrapetyan@gmail.com>
2025-09-07 21:35:19 +0400

    Homework1

---
+++ lesson2/BlokTest.java
@@ -0,0 +1,13 @@
+package lesson2;
+
+public class BlokTest {
+    public static void main(String[] args) {
+        int x;
+        int y = 20;
+        for (x = 0; x < 10; x++) {
+            System.out.println("Знaчeниe х: " + x);
+            System.out.println("Знaчeниe y: " + y);
+            y = y - 2;
+        }
+    }
+}
---
+++ lesson2/Example1.java
@@ -0,0 +1,12 @@
+package lesson2;
+
+public class Example1 {
+    public static void main(String[] args) {
+        int x ;
+        for (x=0; x<10;x++)
+        {
+            System.out.println("x= " + x);
+        }
+        System.out.println("end of for");
+    }
+}
