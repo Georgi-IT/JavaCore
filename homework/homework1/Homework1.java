146ce71603fa33b1fd243845bf35b5f56ed842a7
GeorgiHayrapetyan <gogohayrapetyan@gmail.com>
2025-09-07 22:15:18 +0400

    Homework1

--- homework/homework1/Homework1.java
+++ homework/homework1/Homework1.java
@@ -1,7 +1,33 @@
 package homework.homework1;
 
-public class Homework1 {
-    public static void main(String[] args) {
+import java.sql.SQLOutput;
 
+public class Homework1 {
+    // Կարեն ջան, բոլոր տպելունները փակել եմ որ իրար հետ միանգամից լրիվ չբերի, որը ուզում եք բացեք ստուգեք;
+    public static void main(String[] args) {
+        int x = 10;
+        int y = 15;
+        if (x > y) {
+         //   System.out.println("X > Y");
+            System.out.println();
+        }
+        if (x < y) {
+          //  System.out.println("Y > X");
+            System.out.println();
+        }
+        for (int i = 1; i < 6; i++) {
+          //  System.out.println(i);
+
+        }
+        System.out.println();
+
+        int a = 5;
+        int b = 7;
+       // System.out.println("a + b = " + (a + b));
+        System.out.println();
+        int n = 3;
+        for (int i = 1; i < 11; i++) {
+        //    System.out.println("n * " + i + " = " + n * i);
+        }
     }
 }
